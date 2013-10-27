9e4b39ec36272dceca02e58c0880076f39fe808e
Veselin Todorov <veselin@veselin.bg>
2013-10-27 15:39:46 +0200

    Add `exists` expectation

--- test/exist.test.js
+++ test/exist.test.js
@@ -1,6 +1,44 @@
-describe('nixt#exist', function() {
-  xit('can verify if a file exists');
-  xit('returns an error when the file does not exist');
-  xit('can verify if a directory exists');
-  xit('returns an error when the directory does not exist');
+var fs = require('fs');
+var join = require('path').join;
+var file = join(__dirname, 'tmp', 'exists-file-test');
+var dir = join(__dirname, 'tmp', 'exists-dir-test');
+
+describe('nixt#exists', function() {
+  it('can verify that a file exists', function(done) {
+    nfixt()
+    .writeFile(file)
+    .run('node void.js')
+    .exists(file)
+    .unlink(file)
+    .end(done);
+  });
+
+  it('returns an error when the file does not exist', function(done) {
+    nfixt()
+    .run('node void.js')
+    .exists(file)
+    .end(function(err) {
+      err.should.be.ok;
+      done();
+    });
+  });
+
+  it('can verify that a directory exists', function(done) {
+    nfixt()
+    .mkdir(dir)
+    .run('node void.js')
+    .exists(dir)
+    .rmdir(dir)
+    .end(done);
+  });
+
+  it('returns an error when the directory does not exist', function(done) {
+    nfixt()
+    .run('node void.js')
+    .exists(dir)
+    .end(function(err) {
+      err.should.be.ok;
+      done();
+    });
+  });
 });
