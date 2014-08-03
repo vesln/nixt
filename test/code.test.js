36cb5c8b0c451d23b2b7a7f99174ddb29b6ee79f
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2014-08-02 18:27:28 -0700

    Test against nonzero exit codes

--- test/code.test.js
+++ test/code.test.js
@@ -6,12 +6,22 @@ describe('nixt#code', function() {
     .end(done);
   });
 
-  it('returns na error when the exit code does not match the expected one', function(done) {
+  it('returns an error when the exit code does not match the expected one', function(done) {
     nfixt()
     .run('node code-0.js')
     .code(1)
     .end(function(err) {
       err.message.should.eq('`node code-0.js`: Expected exit code: "1", actual: "0"');
+      done();
+    });
+  });
+
+  it('returns an error when the nonzero exit code does not match the expected one', function(done) {
+    nfixt()
+    .run('node code-1.js')
+    .code(0)
+    .end(function(err) {
+      err.message.should.eq('`node code-1.js`: Expected exit code: "0", actual: "1"');
       done();
     });
   });
